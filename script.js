bcdbdabeadeb54864a66affcd6a238b44db389d6
Imam Noor Khafidzin <imam@noorkhafidzin.com>
2022-06-12 02:15:59 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,110 @@
+const numbers = document.querySelectorAll(".number");
+
+const calculatorScreen = document.querySelector(".calculator-screen");
+const updateScreen = (number) => {
+  calculatorScreen.value = number;
+};
+
+let prevNumber = "";
+let calculatorOperator = "";
+let currentNumber = "0";
+
+const inputNumber = (number) => {
+  if (currentNumber === "0") {
+    currentNumber = number;
+  } else {
+    currentNumber += number;
+  }
+};
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (event) => {
+    inputNumber(event.target.value);
+    updateScreen(currentNumber);
+  });
+});
+
+const operators = document.querySelectorAll(".operator");
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", (event) => {
+    inputOperator(event.target.value);
+  });
+});
+
+const inputOperator = (operator) => {
+  if (calculatorOperator === "") {
+    prevNumber = currentNumber;
+  }
+  calculatorOperator = operator;
+  currentNumber = "0";
+};
+
+const equalSign = document.querySelector(".equal-sign");
+
+equalSign.addEventListener("click", () => {
+  calculate();
+  updateScreen(currentNumber);
+});
+
+const calculate = () => {
+  let result = "";
+  switch (calculatorOperator) {
+    case "+":
+      result = parseFloat(prevNumber) + parseFloat(currentNumber);
+      break;
+    case "-":
+      result = parseFloat(prevNumber) - parseFloat(currentNumber);
+      break;
+    case "*":
+      result = parseFloat(prevNumber) * parseFloat(currentNumber);
+      break;
+    case "/":
+      result = parseFloat(prevNumber) / parseFloat(currentNumber);
+    default:
+      break;
+  }
+  currentNumber = result;
+  calculatorOperator = "";
+};
+
+const clearBtn = document.querySelector(".all-clear");
+
+clearBtn.addEventListener("click", () => {
+  clearAll();
+  updateScreen(currentNumber);
+});
+
+const clearAll = () => {
+  prevNumber = "";
+  calculatorOperator = "";
+  currentNumber = "0";
+};
+
+const decimal = document.querySelector(".decimal");
+
+decimal.addEventListener("click", (event) => {
+  inputDecimal(event.target.value);
+  updateScreen(currentNumber);
+});
+
+inputDecimal = (dot) => {
+  if (currentNumber.includes(".")) {
+    return;
+  }
+  currentNumber += dot;
+};
+
+const percentage = document.querySelector(".percentage");
+
+percentage.addEventListener("click", (event) => {
+  inputPercentage(event.target.value);
+  updateScreen(currentNumber);
+});
+
+inputPercentage = () => {
+  if (currentNumber.includes("%")) {
+    return;
+  }
+  currentNumber = currentNumber / 100;
+};
